c78beb5f283598ca1ef723ca6719ff2ab5a1f652
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2026-03-16 00:50:51 +0200

    Finish DomainLayer with models : Game : id,name,rating and coverUrl GameDetails : name,coverUrl,releaseDate , rating and description

---
+++ app/src/main/java/com/malky/bostatask/domain/GameDetails.kt
@@ -0,0 +1,9 @@
+package com.malky.bostatask.domain
+
+data class GameDetails(
+    val name: String,
+    val coverUrl: String,
+    val releaseDate: String,
+    val rating: Double,
+    val description: String
+)
